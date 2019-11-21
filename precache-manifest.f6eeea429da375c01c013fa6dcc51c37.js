2d261794390f7c666840dbd2fb5acf9d3fd77829
System Administrator <root@CROSSEs-MacBook-Pro.local>
2019-11-21 10:12:26 +0700

    my first

---
+++ precache-manifest.f6eeea429da375c01c013fa6dcc51c37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c8dc099f50f8b1dd634",
+    "url": "/css/chunk-vendors.02b51a40.css"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "e16433f5595720e6a9c77c76188fbb83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1bf8e19bbb4db96fb584",
+    "url": "/js/app.2c798632.js"
+  },
+  {
+    "revision": "2c8dc099f50f8b1dd634",
+    "url": "/js/chunk-vendors.8946e371.js"
+  },
+  {
+    "revision": "e920c28d08e39be02e663d2e076dac81",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
